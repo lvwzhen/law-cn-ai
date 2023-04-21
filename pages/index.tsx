d0053ff6e8e9aeb9f04b531fe76f629a2b75bfcf
lvwzhen <lvwzhen@gmail.com>
2023-04-21 15:33:43 +0800

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Next.js OpenAI Template</title>
+        <title> 中国法律机器人</title>
         <meta
           name="description"
           content="Next.js Template for building OpenAI applications with Supabase."
@@ -26,24 +26,24 @@ export default function Home() {
 
         <div className="py-8 w-full flex items-center justify-center space-x-6">
           <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-            <Link href="https://supabase.com" className="flex items-center justify-center">
-              <p className="text-base mr-2">Built by Supabase</p>
-              <Image src={'/supabase.svg'} width="20" height="20" alt="Supabase logo" />
+            <Link href="https://magickpen.com/" className="flex items-center justify-center">
+              <p className="text-base mr-2">Supported By MagickPen</p>
+              <Image src={'/logo.png'} width="20" height="20" alt="MagickPen logo" />
             </Link>
           </div>
           <div className="border-l border-gray-300 w-1 h-4" />
           <div className="flex items-center justify-center space-x-4">
-            <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
+            {/* <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
               <Link
                 href="https://github.com/supabase/supabase"
                 className="flex items-center justify-center"
               >
                 <Image src={'/github.svg'} width="20" height="20" alt="Github logo" />
               </Link>
-            </div>
+            </div> */}
             <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
               <Link
-                href="https://twitter.com/supabase"
+                href="https://twitter.com/lvwzhen"
                 className="flex items-center justify-center"
               >
                 <Image src={'/twitter.svg'} width="20" height="20" alt="Twitter logo" />
