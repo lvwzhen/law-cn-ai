fbba3f27fffdb15aa71da2324ab308baa2c46156
lvwzhen <lvwzhen@gmail.com>
2023-04-21 23:02:46 +0800

    Fix

--- components/SearchDialog.tsx
+++ components/SearchDialog.tsx
@@ -134,7 +134,7 @@ export function SearchDialog() {
           }
 
           const completionResponse: CreateChatCompletionResponse = JSON.parse(e.data)
-          const text = completionResponse.choices[0].message?.content || "";
+          const text = completionResponse.choices[0].delta?.content || "";
 
           setAnswer((answer) => {
             const currentAnswer = answer ?? ''
