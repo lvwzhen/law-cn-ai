45472be076085e1b2e4e39ac6c9e04de088175ef
lvwzhen <lvwzhen@gmail.com>
2023-04-21 18:00:47 +0800

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,10 +18,6 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <main className={styles.main}>
         <div className={styles.center}>
