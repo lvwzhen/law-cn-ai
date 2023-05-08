b8fa8b9095312d70b59238db5c4184dcb07db86b
lvwzhen <lvwzhen@gmail.com>
2023-05-08 10:55:16 +0800

    Update

--- pages/api/vector-search.ts
+++ pages/api/vector-search.ts
@@ -11,7 +11,6 @@ export const config = {
 }
 
 const openAiKey = process.env.OPENAI_KEY
-const openAiBaseUrl = process.env.OPENAI_BASE_URL || 'api.openai.com';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
@@ -45,7 +44,7 @@ export default async function handler(req: NextRequest) {
 
     // Moderate the content to comply with OpenAI T&C
     const sanitizedQuery = query.trim()
-    const moderationResponse = await fetch('https://' + openAiBaseUrl + '/v1/moderations', {
+    const moderationResponse = await fetch('https://api.openai.com/v1/moderations', {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${openAiKey}`,
@@ -65,7 +64,7 @@ export default async function handler(req: NextRequest) {
       })
     }
 
-    const embeddingResponse = await fetch('https://' + openAiBaseUrl + '/v1/embeddings', {
+    const embeddingResponse = await fetch('https://api.openai.com/v1/embeddings', {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${openAiKey}`,
@@ -144,7 +143,7 @@ export default async function handler(req: NextRequest) {
       stream: true,
     }
 
-    const response = await fetch('https://' + openAiBaseUrl + '/v1/chat/completions', {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${openAiKey}`,
